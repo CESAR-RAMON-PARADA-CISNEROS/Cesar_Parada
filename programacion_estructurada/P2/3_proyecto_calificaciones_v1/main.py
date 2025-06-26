c86423c5840af596f4712ed068a74622c539d1c1
CESAR-RAMON-PARADA-CISNEROS <cesar_3041240389@utd.edu.mx>
2025-06-25 21:31:51 -0600

    commit 1.4
    En este commit es para agregar el archivo principal de llamado main del proyecto 3 versión 1 llamado calificaciones

---
+++ programacion_estructurada/P2/3_proyecto_calificaciones_v1/main.py
@@ -0,0 +1,39 @@
+'''
+Proyecto 3
+Crear un proyecto que permita Gestionar (Administrar) calificaciones; colocar un menu de opciones para agregar, mostrar y calcular
+promedios de las calificaciones de los alumnos
+
+Notas:
+1.- Utilizar funciones y mandar llamar desde otro archivo
+2.- Utilizar listas para almacenar el nombre de un alumno y 3 calificaciones
+'''
+
+import calificaciones
+
+def main():
+    op = True
+    datos = []
+    while op:
+        calificaciones.borrarPantalla()
+        op = calificaciones.menu_principal()
+        match op:
+            case "1":
+                calificaciones.agregarCalificaciones(datos)
+                calificaciones.esperarTecla()
+            case "2":
+                calificaciones.mostrarCalificaciones(datos)
+                calificaciones.esperarTecla()
+            case "3":
+                calificaciones.calcularPromedios(datos)
+                calificaciones.esperarTecla()
+            case "4":
+                calificaciones.borrarPantalla()
+                print("🚪 .::Terminaste de usar el programa::. 🚪")
+                op = False
+            case _:
+                op = True
+                print("⚠️ Opcion no valida, vuelva a intentarlo ⚠️")
+                calificaciones.esperarTecla()
+
+if __name__ == "__main__":
+    main()
